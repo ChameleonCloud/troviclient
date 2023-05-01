325026a57736cab5c97281e470d3cc5467f5070b
Mark Powers <markpowers@uchicago.edu>
2023-05-01 13:10:33 -0500

    Add env loading for required variables

--- README.rst
+++ README.rst
@@ -6,3 +6,16 @@ troviclient
 storing experimental artifacts.
 
 * `Contributing guide <./DEVELOPMENT.rst>`_
+
+# Setup
+
+Set the following environment variables to your desired values
+```
+#!/bin/bash
+export TROVI_KEYCLOAK_URL="https://auth.chameleoncloud.org/auth"
+export TROVI_KEYCLOAK_REALM="chameleon"
+export TROVI_OIDC_CLIENT_ID=""
+export TROVI_OIDC_CLIENT_SECRET=""
+export TROVI_ADMIN=True
+export TROVI_BASE_URL="https://trovi.chameleoncloud.org"
+```
--- troviclient/cli.py
+++ troviclient/cli.py
@@ -1,28 +1,41 @@
 from troviclient import TroviClient
 import click
 import yaml
+import logging
 
 from rich import box
 from rich.console import Console
-from rich.panel import Panel
 from rich.table import Table
-from rich.text import Text
 
 console = Console()
 
-@click.group()
+CONTEXT_SETTINGS = dict(
+    help_option_names=['-h', '--help'],
+    auto_envvar_prefix='TROVI'
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.option("--keycloak_url", required=True, help="")
 @click.option("--keycloak_realm", required=True, help="")
 @click.option("--oidc_client_id", required=True, help="")
 @click.option("--oidc_client_secret", required=True, help="")
+@click.option("--admin", required=True, help="")
+@click.option("--base_url", required=True, help="")
 @click.pass_context
-def cli(ctx, keycloak_url, keycloak_realm, oidc_client_id, oidc_client_secret):
+def cli(ctx, keycloak_url, keycloak_realm, oidc_client_id, oidc_client_secret, admin, base_url):
     ctx.ensure_object(dict)
-    ctx.obj["troviclient"] = TroviClient(keycloak_url, keycloak_realm, oidc_client_id, oidc_client_secret)
+    ctx.obj["troviclient"] = TroviClient(
+        keycloak_url, keycloak_realm,
+        oidc_client_id, oidc_client_secret,
+        admin, base_url
+    )
+
 
 @cli.group("artifact")
 def artifact():
     pass
+
 
 @artifact.command("list", short_help="list artifacts")
 @click.pass_context
@@ -32,13 +45,18 @@ def list_artifacts(ctx):
     table.add_column("Title")
     table.add_column("UUID")
     table.add_column("Created")
+    table.add_column("Owner")
+    table.add_column("Visibility")
     for artifact in artifacts:
         table.add_row(
             artifact["title"],
             artifact["uuid"],
             artifact["created_at"],
+            artifact["owner_urn"][len("urn:trovi:user:"):],
+            artifact["visibility"],
         )
     console.print(table)
+
 
 @artifact.command("show", short_help="list artifacts")
 @click.argument("uuid")
@@ -52,10 +70,12 @@ def show_artifact(ctx, uuid):
         table.add_row(prop, _format_value(value))
     console.print(table)
 
+
 def _format_value(value):
     if isinstance(value, dict) or isinstance(value, list):
         return yaml.dump(value).strip()
     return value
+
 
 def _make_table(*headers, **kwargs):
     kwargs.setdefault("show_header", True)
